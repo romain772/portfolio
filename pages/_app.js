7d515f7631f1bd55fee5d20fbec349732de2dafa
RoM1 <73537345+romain772@users.noreply.github.com>
2022-03-15 11:57:11 +0100

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import HttpsRedirect from 'react-https-redirect';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <HttpsRedirect>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </HttpsRedirect>
     )
 }
 
