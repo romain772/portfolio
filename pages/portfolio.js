3404f6d31fab7a0c8d660743f6ce09b05267d6fe
RoM1 <73537345+romain772@users.noreply.github.com>
2022-03-22 09:13:21 +0100

    Add files via upload

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,8 +2,15 @@ import Title from '../components/Title'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useWindowSize } from "../utils/helpers"
+import Skeleton from 'react-loading-skeleton'
+import { useState } from 'react';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Portfolio(props){
+    const [loading, setLoading] = useState(true)
+    setTimeout(() => {
+        setLoading(false)
+    }, 1000);;
     const size = useWindowSize()
     const portfolio = props.portfolio
     return (
@@ -11,7 +18,7 @@ export default function Portfolio(props){
             <Title title="Portfolio"/>
             <section className={size.width>800?"py-10 min-h-[90vh] w-[90%] m-auto":"py-10 min-h-[90vh] w-[95%] m-auto"}>
                 <div className="w-full">
-                    <h1 className="text-center text-2xl underline">Portfolio</h1>
+                    <h1 className="text-center text-2xl underline">{'Portfolio'}</h1>
                     <div 
                         className={size.width>800?"m-10 flex flex-wrap justify-center":"m-10 flex flex-col justify-center"}
                     >
@@ -23,18 +30,22 @@ export default function Portfolio(props){
                                        className={size.width>800?`w-[400px] h-[400px] m-4 flex flex-col shadow-xl hover:scale-[1.05] duration-150`:
                                        `w-full min-h-[400px] m-auto mt-6 flex flex-col justify-between shadow-xl hover:scale-[1.05] duration-150`}>
                                         <div className="relative">
-                                            <img src={item.image} className="w-full min-h-[270px] duration-150" alt={item.name}/>
+                                            {
+                                                loading?
+                                                <Skeleton height={300}/>:
+                                                <img src={item.image} className="w-full min-h-[270px] duration-150" alt={item.name}/>
+                                            }
                                         </div>
                                         <div className="flex flex-wrap px-2 py-3 absolute">
-                                            {item.tags.map((tag)=>{
+                                            {loading?null:item.tags.map((tag)=>{
                                                 return (
                                                     <span className="text-xs py-1 px-2 rounded-md text-white bg-red-700 hover:bg-yellow-400 mr-1 last:mr-0" key={tag}>{tag}</span>
                                                 )
                                             })}
                                         </div>
                                         <div className="bg-white px-2 min-h-[130px] overflow-auto border-t-[1px] border-slate-700">
-                                            <h1 className="text-xl font-bold m-2 text-center">{item.name}</h1>
-                                            <p className="text-gray-600 mx-2 text-xs">{item.description}</p>
+                                            <h1 className="text-xl font-bold m-2 text-center">{loading?<Skeleton/>:item.name}</h1>
+                                            <p className="text-gray-600 mx-2 text-xs">{loading?<Skeleton/>:item.description}</p>
                                         </div>
                                     </a>
                                 </Link>
