3b91438caf32c2e8b11927e24a9a7cd7a6e02f04
RoM1 <73537345+romain772@users.noreply.github.com>
2022-03-23 16:30:43 +0100

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,26 @@
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from "./Footer"
+import Header from "./Header"
 import Head from "next/head"
-import MobileHeader from "./MobileHeader";
+import MobileHeader from "./MobileHeader"
 import { useWindowSize } from "../utils/helpers"
+import { useState } from 'react';
 
 export default function Layout({ children }) {
   const size = useWindowSize()
+  const [variable , setVariable] = useState(true)
+  function abortAnimation(e){
+    e.preventDefault()
+    if(variable===false)return 
+    setVariable(false)
+  }
     return (
       <>
+        <Head>
+          <meta property="og:description" 
+                content="Mon portfolio." />
+          <meta property="og:image" content="/logo.svg"/>
+        </Head>
+        {/* {variable?<Lines/>:null} */}
         {size.width>800?
         <Header />:
         <MobileHeader/>}
