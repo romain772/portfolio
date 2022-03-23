48e05bbf9c8ca8e8f782f4b4acbdc40aeeb173f4
RoM1 <73537345+romain772@users.noreply.github.com>
2022-03-23 16:31:14 +0100

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-import HttpsRedirect from 'react-https-redirect'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <HttpsRedirect>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </HttpsRedirect>
     )
 }
 
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,6 +1,5 @@
 import Title from '../components/Title'
 import Link from 'next/link'
-import Image from 'next/image'
 import { useWindowSize } from "../utils/helpers"
 import Skeleton from 'react-loading-skeleton'
 import { useState } from 'react';
@@ -25,27 +24,27 @@ export default function Portfolio(props){
                     {portfolio.map((item)=>
                         {                            
                             return(
-                                <Link href={item.link} key={item.id}  >
-                                    <a target="_blank" 
-                                       className={size.width>800?`w-[400px] h-[400px] m-4 flex flex-col shadow-xl hover:scale-[1.05] duration-150`:
-                                       `w-full min-h-[400px] m-auto mt-6 flex flex-col justify-between shadow-xl hover:scale-[1.05] duration-150`}>
-                                        <div className="relative">
-                                            {
-                                                loading?
-                                                <Skeleton height={300}/>:
-                                                <img src={item.image} className="w-full min-h-[270px] duration-150" alt={item.name}/>
-                                            }
-                                        </div>
-                                        <div className="flex flex-wrap px-2 py-3 absolute">
-                                            {loading?null:item.tags.map((tag)=>{
-                                                return (
-                                                    <span className="text-xs py-1 px-2 rounded-md text-white bg-red-700 hover:bg-yellow-400 mr-1 last:mr-0" key={tag}>{tag}</span>
-                                                )
-                                            })}
-                                        </div>
-                                        <div className="bg-white px-2 min-h-[130px] overflow-auto border-t-[1px] border-slate-700">
-                                            <h1 className="text-xl font-bold m-2 text-center">{loading?<Skeleton/>:item.name}</h1>
-                                            <p className="text-gray-600 mx-2 text-xs">{loading?<Skeleton/>:item.description}</p>
+                                <Link href={item.link}>
+                                    <a target="_blank">
+                                        <div key={item.id} className={size.width>800?`w-[400px] h-[400px] m-4 flex flex-col shadow-xl hover:scale-[1.05] duration-150 relative`:
+                                        `w-full min-h-[400px] m-auto mt-6 flex flex-col justify-between shadow-xl hover:scale-[1.05] duration-150 relative`}>
+                                            <div className="flex flex-wrap px-2 py-3 absolute">
+                                                    {loading?null:item.tags.map((tag)=>{
+                                                        return (
+                                                            <span className="text-xs py-1 px-2 rounded-md text-white bg-red-700 hover:bg-yellow-400 mr-1 last:mr-0" key={tag}>{tag}</span>
+                                                        )
+                                                    })}
+                                                </div>
+                                                {
+                                                    loading?
+                                                    <Skeleton height={300}/>:
+                                                    <img src={item.image} className="w-full min-h-[270px] duration-150" alt={item.name}/>
+                                                }
+                                                <div className="bg-white px-2 min-h-[130px] overflow-auto border-t-[1px] border-slate-700">
+                                                    <h1 className="text-xl font-bold m-2 text-center">{loading?<Skeleton/>:item.name}</h1>
+                                                    <p className="text-gray-600 mx-2 text-xs">{loading?<Skeleton/>:item.description}</p>
+                                                </div>
+                                            
                                         </div>
                                     </a>
                                 </Link>
