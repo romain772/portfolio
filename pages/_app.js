0240357c215de77df1499035c9accebc4eb96271
RoM1 <73537345+romain772@users.noreply.github.com>
2022-03-24 14:26:44 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import HttpsRedirect from 'react-https-redirect'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <HttpsRedirect>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </HttpsRedirect>
     )
 }
 
